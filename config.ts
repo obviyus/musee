184c72951354b10fb056af3a7376dcbd63008c1d
Ayaan Zaidi <biblioklept@icloud.com>
2022-07-21 19:46:37 +0530

    feat(exif): sort images by EXIF date

---
+++ config.ts
@@ -0,0 +1,6 @@
+export const config = {
+    title: "Galerie d'Ayaan",
+    description: "Ayaan Zaidi's personal image feed. Mild obsession with aircrafts and shiny things. Mostly shot on a mobile camera.",
+    keywords: "ayaan zaidi, photography, remix",
+    url: "https://gallery.obviy.us",
+}
