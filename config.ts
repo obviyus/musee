20a4268f04f587a5a66f70e8073bea33c78358f4
Ayaan Zaidi <biblioklept@icloud.com>
2022-07-22 12:13:49 +0530

    feat(meta): improve usage of meta-tags

--- config.ts
+++ config.ts
@@ -1,6 +1,9 @@
+import socialImage from './images/social_preview.jpeg'
+
 export const config = {
     title: "Galerie d'Ayaan",
     description: "Ayaan Zaidi's personal image feed. Mild obsession with aircrafts and shiny things. Mostly shot on a mobile camera.",
     keywords: "ayaan zaidi, photography, remix",
     url: "https://gallery.obviy.us",
+    socialPreviewImage: socialImage,
 }
