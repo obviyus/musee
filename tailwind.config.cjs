9b9abe3e77e26b1e166e6b65cb03e6dd1d08a022
Ayaan Zaidi <zaidi@uplause.io>
2022-10-28 15:22:17 +0530

    feat(v2): init

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: [
-		'./app/**/*.{js,ts,jsx,tsx}',
-	],
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {},
 	},
