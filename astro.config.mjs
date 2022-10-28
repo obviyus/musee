045e293b89e1176a7fe06c078e2ec1951e1b39f8
Ayaan Zaidi <zaidi@uplause.io>
2022-10-28 15:31:36 +0530

    fix(config): remove faulty xo rule

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,11 @@
-import { defineConfig } from 'astro/config.mjs';
-
-// https://astro.build/config
+import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-
-// https://astro.build/config
 import image from '@astrojs/image';
-
-// https://astro.build/config
 import critters from 'astro-critters';
 import purgecss from 'astro-purgecss';
 import prefetch from '@astrojs/prefetch';
-
-// https://astro.build/config
 import react from '@astrojs/react';
 
-// https://astro.build/config
 export default defineConfig({
 	integrations: [
 		tailwind(),
