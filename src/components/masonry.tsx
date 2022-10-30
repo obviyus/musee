b3bce485787f8b36c65dd442f900b6fab0a522aa
Ayaan Zaidi <zaidi@uplause.io>
2022-10-30 11:00:22 +0530

    feat(img): do not lazy load first 4 images

--- src/components/masonry.tsx
+++ src/components/masonry.tsx
@@ -41,7 +41,7 @@ export function MasonryGrid(props: { images: sortedImage[] }) {
 								src={image[1].thumbnail.src as string}
 								width={image[1].thumbnail.width as number}
 								height={image[1].thumbnail.height as number}
-								loading={'lazy'}
+								loading={index > 4 ? 'lazy' : 'eager'}
 								className={
 									'transition ease-in-out delay-75 hover:-translate-y-1 hover:scale-110 duration-300'
 								}
