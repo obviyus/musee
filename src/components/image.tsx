7d3c7a11f3fad7da030d0228ccdf13b154b84d7e
Ayaan Zaidi <zaidi@uplause.io>
2023-05-07 15:19:20 +0530

    fix(layout): link to original image on BaseImage

--- src/components/image.tsx
+++ src/components/image.tsx
@@ -27,13 +27,15 @@ export function BaseImage(props: Props) {
 					stiffness: 260,
 					damping: 20,
 				}}>
-				<img
-					src={src}
-					alt={slug}
-					className={
-						'w-full max-h-[80vh] object-contain shadow-2xl rounded-xl border-slate-50 border-2 transition ease-in-out delay-75 hover:-translate-y-1 hover:scale-110 duration-300'
-					}
-				/>
+				<a href={src} target={'_blank'} rel={'noopener noreferrer'}>
+					<img
+						src={src}
+						alt={slug}
+						className={
+							'w-full max-h-[80vh] object-contain shadow-2xl rounded-xl border-slate-50 border-2 transition ease-in-out delay-75 hover:-translate-y-1 hover:scale-110 duration-300'
+						}
+					/>
+				</a>
 			</motion.div>
 
 			<h1 className={'font-bold text-slate-700 mt-8'}>{daysAgo(date)}</h1>
