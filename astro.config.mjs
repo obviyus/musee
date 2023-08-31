cc44f452f0b065db7c429286f8146da4ad81cf08
Ayaan Zaidi <zaidi@uplause.io>
2023-08-31 21:01:32 +0530

    feat(v3): migrate to Astro V3

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,9 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import image from '@astrojs/image';
 import critters from 'astro-critters';
 import purgecss from 'astro-purgecss';
 import react from '@astrojs/react';
 
 export default defineConfig({
-	integrations: [
-		tailwind(),
-		image({
-			serviceEntryPoint: '@astrojs/image/sharp',
-		}),
-		critters(),
-		purgecss(),
-		react(),
-	],
+	integrations: [tailwind(), critters(), purgecss(), react()],
 });
