0062d25f46bff2311f056d0ea298574b964ca1dd
Ayaan Zaidi <zaidi@uplause.io>
2025-07-30 20:46:36 +0530

    feat: remove react related code, ship ZERO JS to client

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
-import react from "@astrojs/react";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 import critters from "astro-critters";
 import purgecss from "astro-purgecss";
 
 export default defineConfig({
-	integrations: [critters(), purgecss(), react()],
+	integrations: [critters(), purgecss()],
 
 	vite: {
 		plugins: [tailwindcss()],
