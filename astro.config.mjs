d1bd9a1fcb4a3b53668da71ba70dca29116c615b
Ayaan Zaidi <zaidi@uplause.io>
2025-08-08 09:48:31 +0530

    chore: remove unused astro-purgecss integration and update package.json

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,11 @@
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 import critters from "astro-critters";
-import purgecss from "astro-purgecss";
 import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
 	site: "https://gallery.obviy.us/",
-	integrations: [critters(), purgecss(), sitemap()],
+	integrations: [critters(), sitemap()],
 
 	vite: {
 		plugins: [tailwindcss()],
