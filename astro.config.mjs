d9ab0652fd22820069fd719e96af7be2c97a3287
Ayaan Zaidi <zaidi@uplause.io>
2025-08-08 09:42:59 +0530

    feat: add robots.txt and sitemap generation

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,11 @@ import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 import critters from "astro-critters";
 import purgecss from "astro-purgecss";
+import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
-	integrations: [critters(), purgecss()],
+	site: "https://gallery.obviy.us/",
+	integrations: [critters(), purgecss(), sitemap()],
 
 	vite: {
 		plugins: [tailwindcss()],
