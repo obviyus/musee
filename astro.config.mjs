0bb5a408331d72be4ed203825800c2396041f7e3
Ayaan Zaidi <zaidi@uplause.io>
2025-12-24 21:45:03 +0530

    chore: trim deps

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,8 @@
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
-import critters from "astro-critters";
-import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
 	site: "https://gallery.obviy.us/",
-	integrations: [critters(), sitemap()],
 
 	vite: {
 		plugins: [tailwindcss()],
